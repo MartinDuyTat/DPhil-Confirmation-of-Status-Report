d3d1690b93e2fa36df27cbbcb12424f5b103f46d
Martin Duy Tat <martin.tat@physics.ox.ac.uk>
2023-02-22 19:27:01 +0000

    Minor changes after comments from Guy

--- texpile.sh
+++ texpile.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 f=ConfirmationOfStatusReport
+#f=DPhilThesisOutline
 if pdflatex $f.tex
 then
   bibtex $f
   pdflatex $f.tex
   pdflatex $f.tex
 fi
-rm $f.{aux,log,bbl,blg,log,out}
+rm $f.{aux,log,bbl,blg,out}
